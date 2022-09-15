06808b3440a803a24ab0d5cb1b8534abbf882101
samnext <samnext9@gmail.com>
2022-09-15 17:55:45 +0300

    lesson2 - решён

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -4,6 +4,8 @@ package lesson2.task2
 
 import lesson1.task1.sqr
 import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 /**
  * Пример
@@ -73,7 +75,11 @@ fun daysInMonth(month: Int, year: Int): Int {
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = TODO()
+): Boolean {
+    val distBetweenCenter = sqrt((x2 - x1).pow(2.0) + (y2 - y1).pow(2.0))
+    return r1 + distBetweenCenter <= r2
+}
+
 
 /**
  * Средняя (3 балла)
